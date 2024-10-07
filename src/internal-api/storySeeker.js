ada1918524fbbdc9a3560b752e9b30daa434b4a3
matej-pc <vanikmatej99@gmail.com>
2024-10-07 22:31:46 +0200

    add logging

--- src/internal-api/storySeeker.js
+++ src/internal-api/storySeeker.js
@@ -2,17 +2,20 @@ import { generateSugestions } from '../external-api/openaiClient.js';
 import { createMessage } from '../utils/messageHandlers.js';
 
 export async function handleGenerateSuggestion(res, parsedMessage) {
-    const { requestId, query } = parsedMessage;
-  
-    console.log('Generating suggestions...');
-    const response = await generateSugestions(requestId, query);
-  
-    const parsedResponse = JSON.parse(response.choices[0].message.content);
-    const { suggestions } = parsedResponse;
-  
-    res.status(200).json(
-      createMessage(requestId, 'RESPONSE_GENERATE_SUGGESTION', {
-        suggestions: suggestions,
-      })
-    );
-  }
+  const { requestId, query } = parsedMessage;
+
+  console.log('Generating suggestions...');
+  const response = await generateSugestions(requestId, query);
+
+  console.log('Parsing: ${response}');
+  const parsedResponse = JSON.parse(response.choices[0].message.content);
+  const { suggestions } = parsedResponse;
+
+  console.log(`Sending response ${suggestions}`);
+
+  res.status(200).json(
+    createMessage(requestId, 'RESPONSE_GENERATE_SUGGESTION', {
+      suggestions: suggestions,
+    })
+  );
+}
