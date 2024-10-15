5263d5297036d78b892c67ac3355e796ebd84599
matej-pc <vanikmatej99@gmail.com>
2024-10-15 20:06:40 +0200

    test

--- src/services/openaiClient.js
+++ src/services/openaiClient.js
@@ -27,7 +27,147 @@ export async function generateSugestions(query) {
         type: 'json_schema',
         json_schema: {
           name: 'RESPONSE_GENERATE_SUGGESTION',
-          schema: compiledSchema.schema,
+          schema: {
+            type: 'object',
+            properties: {
+              suggestions: {
+                type: 'object',
+                properties: {
+                  books: {
+                    type: 'array',
+                    items: {
+                      type: 'object',
+                      properties: {
+                        name: {
+                          type: 'string',
+                          description: 'Full name of the book.',
+                        },
+                        coverImage: {
+                          type: 'string',
+                          description:
+                            'Link to an open-source cover image of the book.',
+                        },
+                        link: {
+                          type: 'string',
+                          description: 'Link to the wiki page of the book.',
+                        },
+                        shortDesc: {
+                          type: 'string',
+                          description:
+                            'Short description of plot. Max two sentences.',
+                        },
+                        plot: {
+                          type: 'string',
+                          description: 'Spoiler free plot.',
+                        },
+                        genre: {
+                          type: 'string',
+                          description: 'Genre of the book.',
+                        },
+                        rating: {
+                          type: 'string',
+                          description: 'Rating based on reviews.',
+                        },
+                        author: {
+                          type: 'string',
+                          description: 'Author of the book.',
+                        },
+                        ISBN: {
+                          type: 'string',
+                          description: 'ISBN code of the book.',
+                        },
+                        pageCount: {
+                          type: 'string',
+                          description: 'Number of pages of the book.',
+                        },
+                      },
+                      required: [
+                        'name',
+                        'coverImage',
+                        'link',
+                        'shortDesc',
+                        'plot',
+                        'genre',
+                        'rating',
+                        'author',
+                        'ISBN',
+                        'pageCount',
+                      ],
+                      additionalProperties: false,
+                    },
+                  },
+                  movies: {
+                    type: 'array',
+                    items: {
+                      type: 'object',
+                      properties: {
+                        name: {
+                          type: 'string',
+                          description: 'Full name of the movie title.',
+                        },
+                        coverImage: {
+                          type: 'string',
+                          description:
+                            'Link to an open-source cover image of the movie.',
+                        },
+                        link: {
+                          type: 'string',
+                          description: 'Link to the wiki page of the movie.',
+                        },
+                        shortDesc: {
+                          type: 'string',
+                          description:
+                            'Short description of plot. Max two sentences.',
+                        },
+                        plot: {
+                          type: 'string',
+                          description: 'Spoiler free plot.',
+                        },
+                        genre: {
+                          type: 'string',
+                          description: 'Genre of the movie.',
+                        },
+                        rating: {
+                          type: 'string',
+                          description: 'Rating based on reviews.',
+                        },
+                        director: {
+                          type: 'string',
+                          description: 'Director of the movie.',
+                        },
+                        boxOffice: {
+                          type: 'string',
+                          description: 'Net profit of the movie.',
+                        },
+                        runtime: {
+                          type: 'string',
+                          description: 'How long the movie is.',
+                        },
+                      },
+                      required: [
+                        'name',
+                        'coverImage',
+                        'link',
+                        'shortDesc',
+                        'plot',
+                        'genre',
+                        'rating',
+                        'director',
+                        'boxOffice',
+                        'runtime',
+                      ],
+                      additionalProperties: false,
+                    },
+                  },
+                },
+                required: ['books', 'movies'],
+                additionalProperties: false,
+              },
+            },
+            required: ['suggestions'],
+            additionalProperties: false,
+          },
+
           strict: true,
         },
       },
